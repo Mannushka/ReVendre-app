648b0ff0826eeed55e176271d3979b7b412ec2ab
Mannushka <zaimar1910@gmail.com>
2025-03-10 22:39:20 +0800

    add input properties

--- app/components/AppTextInput.tsx
+++ app/components/AppTextInput.tsx
@@ -1,8 +1,16 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { View, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TextInputChangeEventData,
+  NativeSyntheticEvent,
+} from "react-native";
 import Colors from "../utils/Colors";
 import defaultStyles from "../utils/Styles";
+
 type IconName =
+  | "lock"
   | "symbol"
   | "function"
   | "key"
@@ -24,14 +32,46 @@ type IconName =
 interface AppTextInputProps {
   icon?: IconName;
   placeholder?: string;
+  autoCapitalize?: "none" | "words" | "characters" | "sentences";
+  autoCorrect?: boolean;
+  keyboardType?:
+    | "default"
+    | "number-pad"
+    | "decimal-pad"
+    | "numeric"
+    | "email-address"
+    | "phone-pad"
+    | "url";
+
+  textContentType?:
+    | "none"
+    | "emailAddress"
+    | "countryName"
+    | "familyName"
+    | "givenName"
+    | "password"
+    | "username";
+
+  onChangeText: (text: string) => void;
 }
 export const AppTextInput: React.FC<AppTextInputProps> = (props) => {
   return (
     <View style={styles.container}>
-      {props.icon && <MaterialCommunityIcons size={16} name={props.icon} />}
+      {props.icon && (
+        <MaterialCommunityIcons
+          style={styles.icon}
+          size={16}
+          name={props.icon}
+        />
+      )}
       <TextInput
         style={defaultStyles.text}
         placeholder={props.placeholder && props.placeholder}
+        autoCapitalize={props.autoCapitalize && props.autoCapitalize}
+        autoCorrect={props.autoCorrect && props.autoCorrect}
+        keyboardType={props.keyboardType && props.keyboardType}
+        textContentType={props.textContentType && props.textContentType}
+        onChangeText={props.onChangeText}
       />
     </View>
   );
@@ -39,12 +79,15 @@ export const AppTextInput: React.FC<AppTextInputProps> = (props) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.GRAY_LIGHT300,
+    backgroundColor: Colors.WHITE,
     borderRadius: 25,
     flexDirection: "row",
     width: "80%",
     padding: 15,
     marginVertical: 10,
     alignItems: "center",
+  },
+  icon: {
+    marginRight: 5,
   },
 });
