3c99ccf43b4728ad613e0a9222b38364d6559112
Mannushka <zaimar1910@gmail.com>
2025-03-11 22:37:27 +0800

    implement formik

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -4,37 +4,47 @@ import { AppTextInput } from "../components/AppTextInput";
 import { ButtonComponent } from "../components/ButtonComponent";
 import Colors from "../utils/Colors";
 import { useState } from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 export const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
   return (
     <Screen>
-      <View style={styles.container}>
-        <AppTextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          icon="email"
-          keyboardType="email-address"
-          placeholder="Email"
-          textContentType="emailAddress"
-          onChangeText={(text: string) => setEmail(text)}
-        />
-        <AppTextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          icon="lock"
-          keyboardType="email-address"
-          placeholder="Password"
-          textContentType="password"
-          onChangeText={(text: string) => setPassword(text)}
-        />
-        <ButtonComponent
-          title="Log in"
-          onClick={() => console.log(email, password)}
-          color={Colors.BLACK}
-        />
-      </View>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <View style={styles.container}>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              placeholder="Email"
+              textContentType="emailAddress"
+              // onChangeText={(text: string) => setEmail(text)}
+              onChangeText={handleChange("email")}
+            />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              keyboardType="email-address"
+              placeholder="Password"
+              textContentType="password"
+              onChangeText={handleChange("password")}
+            />
+            <ButtonComponent
+              title="Log in"
+              onClick={handleSubmit}
+              color={Colors.BLACK}
+            />
+          </View>
+        )}
+      </Formik>
     </Screen>
   );
 };
