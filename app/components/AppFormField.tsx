4ab3ac83e21b9d674604d35673a0f0cc0f750273
Mannushka <zaimar1910@gmail.com>
2025-03-15 22:57:45 +0800

    move types into separate files

--- app/components/AppFormField.tsx
+++ app/components/AppFormField.tsx
@@ -2,26 +2,10 @@ import { AppTextInput } from "./AppTextInput";
 import { View, StyleSheet } from "react-native";
 import { ErrorMessage } from "./ErrorMessage";
 import { useFormikContext, FormikTouched } from "formik";
-type IconName = "lock" | "email" | "image" | "text";
-type Keyboard =
-  | "default"
-  | "number-pad"
-  | "decimal-pad"
-  | "numeric"
-  | "email-address"
-  | "phone-pad"
-  | "url";
-
-type TextContent =
-  | "none"
-  | "emailAddress"
-  | "countryName"
-  | "familyName"
-  | "givenName"
-  | "password"
-  | "username";
-
-type AutoCapitalize = "none" | "words" | "characters" | "sentences";
+import { IconName } from "../types/IconName";
+import { Keyboard } from "../types/Keyboard";
+import { TextContent } from "../types/TextContent";
+import { AutoCapitalize } from "../types/AutoCapitalize";
 
 interface AppFormFieldProps {
   fieldName: string;
@@ -30,7 +14,6 @@ interface AppFormFieldProps {
   autoCapitalize?: AutoCapitalize;
   autoCorrect?: boolean;
   keyboardType?: Keyboard;
-
   textContentType?: TextContent;
 }
 export const AppFormField: React.FC<AppFormFieldProps> = ({
