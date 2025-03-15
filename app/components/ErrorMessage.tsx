46a5b5fe4edfda883a8e9baaf5b3c0a23fe47cd5
Mannushka <zaimar1910@gmail.com>
2025-03-15 23:25:50 +0800

    refactor

--- app/components/ErrorMessage.tsx
+++ app/components/ErrorMessage.tsx
@@ -3,9 +3,9 @@ import Colors from "../utils/Colors";
 interface ErrorMessageProps {
   error: string | undefined;
 }
-export const ErrorMessage: React.FC<ErrorMessageProps> = (props) => {
-  if (!props.error) return null;
-  return <Text style={styles.error}>{props.error}</Text>;
+export const ErrorMessage: React.FC<ErrorMessageProps> = ({ error }) => {
+  if (!error) return null;
+  return <Text style={styles.error}>{error}</Text>;
 };
 
 const styles = StyleSheet.create({ error: { color: Colors.RED } });
