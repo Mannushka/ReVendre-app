6584f87d9dabfde9ca8d269e61c7ad87b8aaf746
Mannushka <zaimar1910@gmail.com>
2025-03-15 23:45:51 +0800

    create submit button

---
+++ app/components/SubmitButton.tsx
@@ -0,0 +1,17 @@
+import { ButtonComponent } from "./ButtonComponent";
+import { useFormikContext } from "formik";
+interface SubmitButtonProps {
+  title: string;
+  color?: string;
+}
+
+export const SubmitButton: React.FC<SubmitButtonProps> = ({ title, color }) => {
+  const { handleSubmit } = useFormikContext();
+  return (
+    <ButtonComponent
+      title={title}
+      color={color}
+      onPress={handleSubmit}
+    ></ButtonComponent>
+  );
+};
