a61406e8b9e3415e9e6199649b828d6cb08072aa
Mannushka <zaimar1910@gmail.com>
2025-03-22 22:08:18 +0800

    finish edit listing form

--- app/screens/ListingEditScreen.tsx
+++ app/screens/ListingEditScreen.tsx
@@ -1,5 +1,6 @@
 import { Screen } from "../components/Screen";
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+import { AppFormPicker } from "../components/forms/AppFormPicker";
 import { View, StyleSheet } from "react-native";
 import * as yup from "yup";
 import Colors from "../utils/Colors";
@@ -7,14 +8,31 @@ import Colors from "../utils/Colors";
 export const ListingEditScreen = () => {
   const initialValues = {
     title: "",
-    price: 0,
+    price: "",
+    category: null,
     description: "",
   };
   const validationSchema = yup.object().shape({
     title: yup.string().required().min(1).label("Title"),
-    price: yup.number().required().min(1).max(10000),
-    description: yup.string().optional(),
+    price: yup.number().required().min(1).max(10000).label("Price"),
+    category: yup.object().required().nullable().label("Category"),
+    description: yup.string().label("Description"),
   });
+
+  const categories = [
+    {
+      label: "Furniture",
+      value: 1,
+    },
+    {
+      label: "Clothing",
+      value: 2,
+    },
+    {
+      label: "Electronics",
+      value: 3,
+    },
+  ];
   return (
     <Screen>
       <AppForm
@@ -25,6 +43,11 @@ export const ListingEditScreen = () => {
         <View style={styles.container}>
           <AppFormField fieldName="title" placeholder="Title"></AppFormField>
           <AppFormField fieldName="price" placeholder="Price"></AppFormField>
+          <AppFormPicker
+            items={categories}
+            fieldName="category"
+            placeholder="Category"
+          ></AppFormPicker>
           <AppFormField
             fieldName="description"
             placeholder="Description"
