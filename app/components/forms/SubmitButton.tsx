c956be80e25d1d5b5d9ce1c35ac84a804bb3dbf4
Mannushka <zaimar1910@gmail.com>
2025-03-23 22:35:52 +0800

    add width and bg color props

--- app/components/forms/SubmitButton.tsx
+++ app/components/forms/SubmitButton.tsx
@@ -1,16 +1,26 @@
+import { DimensionValue } from "react-native";
 import { ButtonComponent } from "../ButtonComponent";
 import { useFormikContext } from "formik";
 interface SubmitButtonProps {
-  title: string;
   color?: string;
+  backgroundColor?: string;
+  title: string;
+  width?: DimensionValue;
 }
 
-export const SubmitButton: React.FC<SubmitButtonProps> = ({ title, color }) => {
+export const SubmitButton: React.FC<SubmitButtonProps> = ({
+  backgroundColor,
+  color,
+  title,
+  width,
+}) => {
   const { handleSubmit } = useFormikContext();
   return (
     <ButtonComponent
-      title={title}
+      backgroundColor={backgroundColor}
       color={color}
+      title={title}
+      buttonWidth={width}
       onPress={handleSubmit}
     ></ButtonComponent>
   );
