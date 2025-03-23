75dcf4a1e3dadd58883b5f00e91c9bbcee919ba2
Mannushka <zaimar1910@gmail.com>
2025-03-23 13:06:16 +0800

    fix imports

--- app/screens/ListingDetailsScreen.tsx
+++ app/screens/ListingDetailsScreen.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native";
 import { ListingCard } from "../components/ListingCard";
-import { ListItem } from "../components/ListItem";
+import { ListItem } from "../components/lists/ListItem";
 import { Screen } from "../components/Screen";
 import Colors from "../utils/Colors";
 
