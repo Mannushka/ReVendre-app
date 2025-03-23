8566709dfaf597851fec559d5e353ea273a29756
Mannushka <zaimar1910@gmail.com>
2025-03-23 22:33:34 +0800

    set button width dynamically

--- app/components/ButtonComponent.tsx
+++ app/components/ButtonComponent.tsx
@@ -1,29 +1,38 @@
 import React from "react";
-import { Button, View, StyleSheet } from "react-native";
+import { Button, View, StyleSheet, DimensionValue } from "react-native";
 interface ButtonProps {
-  title: string;
-  color?: string;
   backgroundColor?: string;
+  buttonWidth?: DimensionValue;
+  color?: string;
+  title: string;
   onPress: () => void;
 }
 export const ButtonComponent: React.FC<ButtonProps> = ({
-  title,
-  color,
   backgroundColor,
+  buttonWidth,
+  color,
+  title,
   onPress,
 }) => {
+  const width = buttonWidth ? buttonWidth : "100%";
   return (
-    <View style={[styles.button, { backgroundColor: backgroundColor }]}>
-      <Button title={title} color={color} onPress={onPress} />
+    <View
+      style={[
+        styles.button,
+        { backgroundColor: backgroundColor },
+        { width: width },
+      ]}
+    >
+      <Button color={color} title={title} onPress={onPress} />
     </View>
   );
 };
 const styles = StyleSheet.create({
   button: {
-    borderRadius: "10%",
-    padding: 1,
-    width: 160,
+    borderRadius: "8%",
+    padding: 10,
     opacity: 0.8,
     alignSelf: "center",
+    marginTop: 20,
   },
 });
