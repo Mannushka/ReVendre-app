a53b84fa4a1f59a4091cf7730db79e9b02417363
Mannushka <zaimar1910@gmail.com>
2025-04-05 19:06:36 +0800

    create logo

---
+++ app/components/Logo.tsx
@@ -0,0 +1,28 @@
+import { View, StyleSheet, Image } from "react-native";
+import Colors from "../utils/Colors";
+
+export const Logo = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/revendre-logo.png")}
+        style={styles.image}
+      ></Image>
+      ;
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    shadowColor: Colors.SHADOW_BLACK,
+    shadowOffset: { width: -2, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: "50%",
+  },
+});
