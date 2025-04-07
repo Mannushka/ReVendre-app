e37500431c420f9fb701df6af794e35539555b2f
Mannushka <zaimar1910@gmail.com>
2025-04-07 21:59:35 +0800

    remove old code

--- app/components/ImageInput.tsx
+++ app/components/ImageInput.tsx
@@ -1,10 +1,6 @@
 import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { ImageInputIcon } from "./ImageInputIcon";
 import { Screen } from "./Screen";
-import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
-
-import Colors from "../utils/Colors";
 
 interface ImageInputProps {
   imageUri: string | null;
@@ -14,20 +10,6 @@ export const ImageInput: React.FC<ImageInputProps> = ({
   imageUri,
   onChangeImage,
 }) => {
-  // const [imageUri, setImageUri] = useState<string | null>(null);
-
-  // const pickImage = async () => {
-  //   const result = await ImagePicker.launchImageLibraryAsync({
-  //     mediaTypes: "images",
-  //     allowsEditing: true,
-  //     aspect: [5, 4],
-  //   });
-  //   console.log(result);
-  //   if (!result.canceled) {
-  //     setImageUri(result.assets[0].uri);
-  //   }
-  // };
-
   return (
     <Screen>
       <View style={styles.container}>
