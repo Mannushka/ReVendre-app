c24dfc5baf3299c17200dd23bec81332a0451cf3
Mannushka <zaimar1910@gmail.com>
2025-06-07 22:44:41 +0800

    create use location hook

---
+++ app/hooks/useLocation.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { UserLocation } from "../types/UserLocation";
+import * as Location from "expo-location";
+export const useLocation = () => {
+  const [location, setLocation] = useState<UserLocation | null>(null);
+  try {
+    const getLocation = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") return;
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({});
+      setLocation({ latitude, longitude });
+    };
+    useEffect(() => {
+      getLocation();
+    }, []);
+  } catch (error) {
+    console.log(error);
+  }
+  return location;
+};
