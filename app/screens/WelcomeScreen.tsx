3201b1509df2d52d7e521c430780329a2c002bb7
Mannushka <zaimar1910@gmail.com>
2025-06-10 23:26:47 +0800

    add logic for navigation, delete old code

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,13 +1,18 @@
-import { StyleSheet, Text, View, ImageBackground, Button } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import { SafeAreaView } from "react-native";
 import { ButtonComponent } from "../components/ButtonComponent";
 import Colors from "../utils/Colors";
 import { Logo } from "../components/Logo";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AuthRootStackParamList } from "../types/NavigationTypes";
+import { useNavigation } from "@react-navigation/native";
 
-export default function WelcomeScreen() {
-  // const image = {
-  //   uri: "https://images.unsplash.com/photo-1504198458649-3128b932f49e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  // }; // old image
+type WelcomeScreenNavigationProp = NativeStackNavigationProp<
+  AuthRootStackParamList,
+  "Welcome"
+>;
+export function WelcomeScreen() {
+  const navigation = useNavigation<WelcomeScreenNavigationProp>();
   return (
     <ImageBackground
       source={require("../assets/welcome-screen-bg-img.avif")}
@@ -25,14 +30,14 @@ export default function WelcomeScreen() {
             color={Colors.WHITE}
             backgroundColor={Colors.BUTTON_CORAL}
             buttonWidth={"90%"}
-            onPress={() => console.log("log in btn clicked")}
+            onPress={() => navigation.navigate("Login")}
           />
           <ButtonComponent
             title="I'm a new user"
             color={Colors.WHITE}
             backgroundColor={Colors.BUTTON_CORAL}
             buttonWidth={"90%"}
-            onPress={() => console.log("new user btn clicked")}
+            onPress={() => navigation.navigate("Register")}
           />
         </View>
       </SafeAreaView>
