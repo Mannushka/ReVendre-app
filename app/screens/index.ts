4cd1179c6f18f67482d0938732d9012f5f11db20
Mannushka <zaimar1910@gmail.com>
2025-06-11 00:07:17 +0800

    create index file for exporting screens

---
+++ app/screens/index.ts
@@ -0,0 +1,8 @@
+export { WelcomeScreen } from "./WelcomeScreen";
+export { LoginScreen } from "./LoginScreen";
+export { RegisterScreen } from "./RegisterScreen";
+export { ListingEditScreen } from "./ListingEditScreen";
+export { ListingDetailsScreen } from "./ListingDetailsScreen";
+export { MessagesScreen } from "./MessagesScreen";
+export { ProfileScreen } from "./ProfileScreen";
+export { ViewImageScreen } from "./ViewImageScreen";
