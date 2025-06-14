7df4c86ee6f7049425142589522b10ec38d2a96d
Mannushka <zaimar1910@gmail.com>
2025-06-14 20:21:20 +0800

    add listings data

---
+++ app/data/Listings.ts
@@ -0,0 +1,24 @@
+export const listings = [
+  {
+    id: 1,
+    title: "Blue Pillow",
+    price: "$20",
+    imageUrl:
+      "https://images.unsplash.com/photo-1570786240066-c0d753711cfe?q=80&w=1762&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    id: 2,
+    title: "Gray Chair",
+    price: "$30",
+    imageUrl:
+      "https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "White Iphone 11",
+    price: "$150",
+    imageUrl:
+      "https://images.unsplash.com/photo-1603891128711-11b4b03bb138?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
