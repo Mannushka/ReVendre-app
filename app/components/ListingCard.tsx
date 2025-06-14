b25ea0de56042c87747b43f7bb8cf5af5a9ab66b
Mannushka <zaimar1910@gmail.com>
2025-06-14 22:12:01 +0800

    add on press event

--- app/components/ListingCard.tsx
+++ app/components/ListingCard.tsx
@@ -1,38 +1,49 @@
 import Colors from "../utils/Colors";
-import { View, StyleSheet, Image, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 interface ListingCardProps {
   title: string;
   subTitle: string;
   image: string;
+  onPress?: () => void;
 }
 
 export const ListingCard: React.FC<ListingCardProps> = ({
   title,
   subTitle,
   image,
+  onPress,
 }) => {
   const imageSource = {
     uri: image,
   };
   return (
-    <View style={styles.container}>
-      <View style={styles.card}>
-        <Image source={imageSource} style={styles.image} />
-        <View style={styles.textContainer}>
-          <View>
-            <Text style={styles.title} numberOfLines={1}>
-              {title}
-            </Text>
-          </View>
-          <View>
-            <Text style={styles.subTitle} numberOfLines={3}>
-              {subTitle}
-            </Text>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Image source={imageSource} style={styles.image} />
+          <View style={styles.textContainer}>
+            <View>
+              <Text style={styles.title} numberOfLines={1}>
+                {title}
+              </Text>
+            </View>
+            <View>
+              <Text style={styles.subTitle} numberOfLines={3}>
+                {subTitle}
+              </Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 const styles = StyleSheet.create({
