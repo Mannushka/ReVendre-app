cb53da4777fb6de5d6973c493b1b058824e7c7df
Mannushka <zaimar1910@gmail.com>
2025-06-14 22:22:43 +0800

    add navigation

--- app/screens/ListingDetailsScreen.tsx
+++ app/screens/ListingDetailsScreen.tsx
@@ -2,28 +2,37 @@ import { View, StyleSheet } from "react-native";
 import { ListingCard } from "../components/ListingCard";
 import { ListItem } from "../components/lists/ListItem";
 import { Screen } from "../components/Screen";
+import { Listing } from "../types/Listing";
+import { useRoute, RouteProp } from "@react-navigation/native";
+
+type RootStackParamList = {
+  ListingsScreen: undefined;
+  ListingDetailsScreen: {
+    listing: { id: number; title: string; price: string; imageUrl: string };
+  };
+};
+
+type ListingDetailsRouteProp = RouteProp<
+  RootStackParamList,
+  "ListingDetailsScreen"
+>;
 
 export const ListingDetailsScreen = () => {
-  const listing = {
-    title: "Blue Pillow",
-    subTitle: "$20",
-    imageUrl:
-      "https://images.unsplash.com/photo-1570786240066-c0d753711cfe?q=80&w=1762&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  };
-
+  const route = useRoute<ListingDetailsRouteProp>();
+  const { listing } = route.params;
   return (
     <Screen>
       <View style={styles.container}>
         <ListingCard
           title={listing.title}
-          subTitle={listing.subTitle}
+          subTitle={listing.price}
           image={listing.imageUrl}
         />
-        <ListItem
+        {/* <ListItem
           image="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           title="Luis"
           subTitle="Hello world"
-        />
+        /> */}
       </View>
     </Screen>
   );
--- app/screens/ListingsScreen.tsx
+++ app/screens/ListingsScreen.tsx
@@ -2,8 +2,25 @@ import { FlatList } from "react-native";
 import { listings } from "../data/Listings";
 import { ListingCard } from "../components/ListingCard";
 import { Screen } from "../components/Screen";
+import { Listing } from "../types/Listing";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+type RootStackParamList = {
+  ListingsScreen: undefined;
+  ListingDetailsScreen: {
+    listing: { id: number; title: string; price: string; imageUrl: string };
+  };
+};
+// Define the navigation prop type specific to this screen
+type ListingsScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "ListingsScreen",
+  "ListingDetailsScreen"
+>;
 
-const ListingsScreen = () => {
+const ListingsScreen: React.FC<{
+  navigation: ListingsScreenNavigationProp;
+}> = ({ navigation }) => {
   return (
     <Screen>
       <FlatList
@@ -14,7 +31,16 @@ const ListingsScreen = () => {
             title={item.title}
             subTitle={item.price}
             image={item.imageUrl}
-            onPress={() => console.log(item.title)}
+            onPress={() =>
+              navigation.navigate("ListingDetailsScreen", {
+                listing: {
+                  id: item.id,
+                  title: item.title,
+                  price: item.price,
+                  imageUrl: item.imageUrl,
+                },
+              })
+            }
           />
         )}
       />
