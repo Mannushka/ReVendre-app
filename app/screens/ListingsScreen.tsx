8d5f3ee2a03ed60a993448be3e61b70b6b1f25e5
Mannushka <zaimar1910@gmail.com>
2025-06-15 12:30:24 +0800

    refactor

--- app/screens/ListingsScreen.tsx
+++ app/screens/ListingsScreen.tsx
@@ -2,10 +2,9 @@ import { FlatList } from "react-native";
 import { listings } from "../data/Listings";
 import { ListingCard } from "../components/ListingCard";
 import { Screen } from "../components/Screen";
-import { Listing } from "../types/Listing";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { useNavigation } from "@react-navigation/native";
 import routes from "../components/navigators/routes";
+
 type RootStackParamList = {
   ListingsScreen: undefined;
   ListingDetailsScreen: {
@@ -15,7 +14,7 @@ type RootStackParamList = {
     imageUrl: string;
   };
 };
-// Define the navigation prop type specific to this screen
+
 type ListingsScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
   typeof routes.LISTINGS,
