2e7fbac7fef7df99bd83b52b6b80cf955c8fbd23
Mannushka <zaimar1910@gmail.com>
2025-06-15 23:31:23 +0800

    change styling

--- app/screens/ListingDetailsScreen.tsx
+++ app/screens/ListingDetailsScreen.tsx
@@ -1,9 +1,10 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { ListingCard } from "../components/ListingCard";
 import { ListItem } from "../components/lists/ListItem";
 import { Screen } from "../components/Screen";
 import { useRoute, RouteProp } from "@react-navigation/native";
 import routes from "../components/navigators/routes";
+import { ImageItem } from "../components/ImageItem";
 
 type RootStackParamList = {
   ListingsScreen: undefined;
@@ -26,12 +27,17 @@ const ListingDetailsScreen = () => {
   return (
     <Screen>
       <View style={styles.container}>
-        <ListingCard title={title} subTitle={price} image={imageUrl} />
+        {/* <ListingCard title={title} subTitle={price} image={imageUrl} /> */}
         {/* <ListItem
           image="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           title="Luis"
           subTitle="Hello world"
         /> */}
+        <ImageItem imageUri={imageUrl} width={"100%"} height={350} />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{title}</Text>
+          <Text style={styles.text}>{price}</Text>
+        </View>
       </View>
     </Screen>
   );
@@ -41,6 +47,17 @@ const styles = StyleSheet.create({
     // flex: 1,
     // justifyContent: "flex-start",
     // alignItems: "center",
+  },
+  textContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  text: {
+    marginVertical: 30,
+    marginHorizontal: 20,
+    fontSize: 20,
+    fontFamily: "Helvetica-Bold",
   },
 });
 export default ListingDetailsScreen;
