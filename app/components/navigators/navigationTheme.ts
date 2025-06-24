08b9f61034e052b93f12e637c1c2fb083cdfd9ab
Mannushka <zaimar1910@gmail.com>
2025-06-24 22:06:15 +0800

    edit styling

--- app/components/navigators/navigationTheme.ts
+++ app/components/navigators/navigationTheme.ts
@@ -7,5 +7,11 @@ export default {
     ...DefaultTheme.colors,
     primary: Colors.BUTTON_CORAL,
     background: Colors.BEIGE_LIGHT,
+    card: Colors.BUTTON_CORAL,
+    text: Colors.WHITE,
+    border: Colors.BUTTON_CORAL,
+  },
+  fonts: {
+    ...DefaultTheme.fonts,
   },
 };
