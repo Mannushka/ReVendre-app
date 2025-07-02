52fe4cfb80a73bd9c0d53d064753035729b75011
Mannushka <zaimar1910@gmail.com>
2025-07-02 23:00:22 +0800

    add auth

--- app/screens/RegisterScreen.tsx
+++ app/screens/RegisterScreen.tsx
@@ -4,6 +4,7 @@ import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 import * as yup from "yup";
 import Colors from "../utils/Colors";
 import { PageTitle } from "../components/PageTitle";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const RegisterScreen = () => {
   const validationSchema = yup.object().shape({
@@ -11,13 +12,47 @@ const RegisterScreen = () => {
     email: yup.string().required().email().label("Email"),
     password: yup.string().required().min(8).label("Password"),
   });
+
+  const { isLoaded, signUp, setActive } = useSignUp();
+
+  const handleSignUp = async (
+    username: string,
+    emailAddress: string,
+    password: string
+  ) => {
+    if (!isLoaded) return;
+
+    try {
+      await signUp.create({
+        username,
+        emailAddress,
+        password,
+      });
+
+      // Immediately set the session as active without verification
+      // await setActive(); // Assuming this function sets the session as active
+
+      // // Redirect the user to the desired location
+      // router.replace("/");
+      console.log("sign up status", signUp.status);
+
+      if (signUp.status === "complete") console.log("User registered");
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
   return (
     <Screen>
       <PageTitle titleString="Let's create you an account!" />
       <AppForm
         initialValues={{ username: "", email: "", password: "" }}
         validationSchema={validationSchema}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => {
+          const { username, email, password } = values;
+          console.log(email);
+          handleSignUp(username, email, password);
+          console.log("hello");
+        }}
       >
         <View style={styles.container}>
           <AppFormField
@@ -65,3 +100,5 @@ const styles = StyleSheet.create({
   },
 });
 export default RegisterScreen;
+//add email verification
+//add log out button
