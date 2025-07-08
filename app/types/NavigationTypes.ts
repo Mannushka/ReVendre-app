3dfe7e9d53a9be03e3617d7bb0f99cbedb3c981f
Mannushka <zaimar1910@gmail.com>
2025-07-08 23:15:40 +0800

    edit auth navigator type

--- app/types/NavigationTypes.ts
+++ app/types/NavigationTypes.ts
@@ -1,7 +1,10 @@
+import { number } from "yup";
+
 export type AuthRootStackParamList = {
   Welcome: undefined;
   Login: undefined;
   Register: undefined;
+  Main: undefined;
 };
 
 export type ProfileRootStackParamList = {
