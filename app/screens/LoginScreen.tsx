fc98823ccbba587e58bd51e90554ec2b00e4f36b
Mannushka <zaimar1910@gmail.com>
2025-07-09 22:45:34 +0800

    add sign in logic

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -4,12 +4,41 @@ import Colors from "../utils/Colors";
 import * as yup from "yup";
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 import { PageTitle } from "../components/PageTitle";
+import { useSignIn } from "@clerk/clerk-expo";
+import { ErrorMessage } from "../components/forms";
 
-const validationSchema = yup.object().shape({
-  email: yup.string().required().email().label("Email"),
-  password: yup.string().required().min(8).label("Password"),
-});
 const LoginScreen = () => {
+  const validationSchema = yup.object().shape({
+    email: yup.string().required().email().label("Email"),
+    password: yup.string().required().min(8).label("Password"),
+  });
+
+  const { isLoaded, signIn, setActive } = useSignIn();
+
+  const handleSignIn = async (email: string, password: string) => {
+    if (!isLoaded) return;
+    // if (!email || !password)
+    //   return <ErrorMessage error="Please fill out the log in form" />;
+    try {
+      await signIn.create({
+        identifier: email,
+        password: password,
+      });
+
+      // Immediately set the session as active without verification
+      // await setActive(); // Assuming this function sets the session as active
+
+      // // Redirect the user to the desired location
+      // router.replace("/");
+      console.log("sign in status", signIn.status);
+
+      if (signIn.status === "complete") {
+        console.log("User logged in");
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
   return (
     <Screen>
       <PageTitle titleString="Welcome back!" />
@@ -23,8 +52,11 @@ const LoginScreen = () => {
 
       <AppForm
         initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
+        onSubmit={(values) => {
+          const { email, password } = values;
+          handleSignIn(email, password);
+        }}
       >
         <View style={styles.container}>
           <AppFormField
