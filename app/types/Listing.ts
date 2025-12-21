a898c0075c21dc760ac6fe5c4ab627c15080ad00
Mannushka <zaimar1910@gmail.com>
2025-12-21 21:41:32 +0800

    create listing type

---
+++ app/types/Listing.ts
@@ -0,0 +1,16 @@
+import { Category } from "./Category";
+export type Listing = {
+  id: number;
+  title: string;
+  price: string;
+  imageUrl: string;
+};
+//not sure if the type is needed
+
+export type ListingFormValues = {
+  title: string;
+  price: number;
+  category: Category["id"];
+  description: string;
+  images: string[];
+};
