0e30221d02f6c32a0b448f9a643fa61245d5514c
Mannushka <zaimar1910@gmail.com>
2026-02-01 17:43:16 +0800

    add supabase keys types

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,6 @@
 declare module "@env" {
   export const EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
-  export const BACKEND_URL: string;
+  export const SUPABASE_PROJECT_URL: string;
+  export const SUPABASE_PUBLISHABLE_KEY: string;
+  export const SUPABASE_ANON_KEY: string;
 }
